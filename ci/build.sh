7606181dc33a14ad9f95f7fa8257a7fbc23ba7ed
VIFF <atsunaakiya@outlook.com>
2019-08-15 02:03:07 +1000

    bugfix

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,8 @@
+if [ ! -e data ]; then
+    mkdir data
+    mkdir data/indices
+    mkdir data/regions
+fi
 react-scripts build
 cd app
 ../node_modules/typescript/bin/tsc --outDir ./build --esModuleInterop app.ts
